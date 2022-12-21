8e9656370a49b31d32ee48245b7ea502685e648a
root <root@DESKTOP-C0MK0L2.localdomain>
2022-12-21 08:27:55 -0930

    add pint

--- main.c
+++ main.c
@@ -7,31 +7,35 @@
 int main(int argc, char **argv)
 {
 	stack_t *stack = NULL;
-	FILE *fp;
-	char *line = NULL;
-	size_t len = 0;
-	char *opcode;
-	int i;
-	unsigned int line_number = 0;
 	instruction_t instructions[] = {
 		{"push", push},
 		{"pall", pall},
 		{"pint", pint},
 		{NULL, NULL}};
+	char *opcode;
+	int i;
+	unsigned int line_number = 0;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		return (EXIT_FAILURE);
 	}
+
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		return (EXIT_FAILURE);
 	}
+
 	while (getline(&line, &len, fp) != -1)
 	{
+		line_number++;
 		opcode = strtok(line, " \n");
 		if (opcode == NULL || opcode[0] == '#')
 			continue;
@@ -40,10 +44,11 @@ int main(int argc, char **argv)
 		{
 			if (strcmp(instructions[i].opcode, opcode) == 0)
 			{
-				instructions[i].f(&stack, ++line_number);
+				instructions[i].f(&stack, line_number);
 				break;
 			}
 		}
+
 		if (instructions[i].opcode == NULL)
 		{
 			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
@@ -51,6 +56,7 @@ int main(int argc, char **argv)
 			return (EXIT_FAILURE);
 		}
 	}
+
 	free(line);
 	fclose(fp);
 	free_stack(stack);
