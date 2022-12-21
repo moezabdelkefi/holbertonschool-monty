c17902ec776245b3da20c84572fee3b48a5a8282
root <root@DESKTOP-C0MK0L2.localdomain>
2022-12-21 08:22:24 -0930

    add pint

--- func.c
+++ func.c
@@ -53,6 +53,23 @@ void pall(stack_t **stack, unsigned int line_number)
 	}
 	(void)line_number;
 }
+#include "monty.h"
+
+/**
+ * pint - print the value at the top of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: line number in the file
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !*stack)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
+}
 
 /**
  * free_stack - free the memory used by the stack
--- main.c
+++ main.c
@@ -7,31 +7,29 @@
 int main(int argc, char **argv)
 {
 	stack_t *stack = NULL;
-	instruction_t instructions[] = {
-		{"push", push},
-		{"pall", pall},
-		{NULL, NULL}};
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
 	char *opcode;
 	int i;
 	unsigned int line_number = 0;
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{NULL, NULL}};
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		return (EXIT_FAILURE);
 	}
-
-	FILE *fp;
-	char *line = NULL;
-	size_t len = 0;
-
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		return (EXIT_FAILURE);
 	}
-
 	while (getline(&line, &len, fp) != -1)
 	{
 		opcode = strtok(line, " \n");
@@ -46,7 +44,6 @@ int main(int argc, char **argv)
 				break;
 			}
 		}
-
 		if (instructions[i].opcode == NULL)
 		{
 			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
@@ -54,7 +51,6 @@ int main(int argc, char **argv)
 			return (EXIT_FAILURE);
 		}
 	}
-
 	free(line);
 	fclose(fp);
 	free_stack(stack);
